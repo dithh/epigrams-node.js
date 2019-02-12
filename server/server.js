2ea173e0d2fa98bb4f5e877f6b0e8cff422f9d39
unknown <aleksander.hersztek@gmail.com>
2019-02-12 21:33:10 +0100

    patch route was added

--- server/server.js
+++ server/server.js
@@ -61,38 +61,51 @@ app.get(`/epigrams/:id`, (req, res) => {
 
 app.delete(`/epigrams/:id`, (req, res) => {
     let id = req.params.id;
-      if(validateId(id,req, res)){
-    Epigram.findByIdAndDelete(id).then((epigram) => {
-        if (epigram) {
-            res.send({
-                epigram
-            });
-        } else {
-            res.status(404).send({
-                message: `document with that id was not found`
-            })
-        }
-    });
-}
+    if (validateId(id, req, res)) {
+        Epigram.findByIdAndDelete(id).then((epigram) => {
+            if (epigram) {
+                res.send({
+                    epigram
+                });
+            } else {
+                res.status(404).send({
+                    message: `document with that id was not found`
+                })
+            }
+        });
+    }
 });
 
 app.patch(`/epigrams/:id`, (req, res) => {
     const id = req.params.id;
-     if (validateId(id, req, res)){
-         
-     }
-    
+    if (validateId(id, req, res)) {
+        Epigram.findById(id).then((epigram) => {
+            if (epigram) {
+                for (let property in req.body) {
+                    epigram[property] = req.body[property];
+                }
+                epigram.save().then(doc => {
+                    res.send(doc);
+                })
+            } else {
+                res.status(404).send({
+                    message: `document with that id was not found`
+                })
+            }
+        })
+    }
+
 });
 
 app.listen(`3000`);
 
 
-function validateId(id ,req, res) {
+function validateId(id, req, res) {
     if (!ObjectID.isValid(id)) {
         res.status(400).send({
             message: "id is not valid"
         });
         return false;
     }
-  return true ;
+    return true;
 }
